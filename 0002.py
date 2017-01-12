68415c3d8d3ea1ca9e77e5daf15a80e631c89f85
Kousuke Taekuchi <kousuketakeuchi@gmail.com>
2017-01-12 10:15:21 +0900

    Split return each variables.

--- 0002.py
+++ 0002.py
@@ -6,11 +6,8 @@ http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0002
 
 while True:
     try:
-        a = raw_input().split()
-        #print a
-        c = 0
-        for b in a:
-            c = c + int(b)
-        print len(str(c))
+        a, b = raw_input().split()
+        #print a, b
+        print len(str(int(a)+int(b)))
     except EOFError:
         break
